f69210def396c41523aac0e1f97697009bebfc13
Sjain0018 <sj5373557@gmail.com>
2025-08-28 02:23:18 +0530

    fifth

--- Backend/config.js
+++ Backend/config.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({ path: ".env.local" });
 
 const PORT = process.env.PORT || 8000;
-const mongoURL =
-  process.env.MONGO_URL || "mongodb://localhost:27017/lost-and-found";
+const mongoURL = process.env.MONGO_URL || "mongodb://localhost:27017/lost-and-found";
 
 module.exports = { PORT, mongoURL };
